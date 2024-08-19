8697bbd60205d49bd2e950430d3620865e614455
Ceasar082201 <refermosoc@gmail.com>
2024-08-19 15:33:43 +0800

    pabasa kanag mga comments

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
+//Main Menu
 Route::get('/', function () { return view('home'); });
 Route::get('/home', function () { return view('home'); });
 Route::get('/task', function () { return view('task'); });
@@ -10,7 +11,9 @@ Route::get('/schedule', function () { return view('schedule'); });
 Route::get('/user', function () { return view('users/create'); });
 Route::get('/setting', function () { return view('setting'); });
 
-
+//Create User
 Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
 Route::post('/users', [UserController::class, 'store'])->name('users.store');
+
+//Display sa baba na su mga Routes
