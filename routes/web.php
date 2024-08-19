6d6781fd127189bfd92d556313da2978ad3a634c
Ceasar082201 <refermosoc@gmail.com>
2024-08-19 15:51:41 +0800

    dagdag bawas

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,7 @@ use App\Http\Controllers\ScheduleController;
 //Main Menu
 Route::get('/', function () { return view('home'); });
 Route::get('/home', function () { return view('home'); });
-Route::get('/task', function () { return view('task'); });
-Route::get('/schedule', function () { return view('schedule'); });
-Route::get('/user', function () { return view('users/create'); });
-Route::get('/setting', function () { return view('setting'); });
+
 
 //Create User
 Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
@@ -19,7 +16,5 @@ Route::get('/users/delete', [UserController::class, 'delete'])->name('users.dele
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
 Route::post('/users', [UserController::class, 'store'])->name('users.store');
 
-//Display sa baba na su mga Routes
-
-
+//Display sa baba na su mga Routes, sabing sa baba ngani
 Route::get('/schedule', [ScheduleController::class, 'index'])->name('users.schedule');
