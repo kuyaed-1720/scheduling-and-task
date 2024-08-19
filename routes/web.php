06617ae979812d5b515eccc1daaac5586ef07570
kuyaed-1720 <kuya.ed20@gmail.com>
2024-08-19 15:53:49 +0800

    Co-authored-by: Ceasar082201 <refermosoc@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -4,19 +4,16 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ScheduleController;
 
+// Home
 Route::get('/', function () { return view('home'); });
 Route::get('/home', function () { return view('home'); });
-Route::get('/task', function () { return view('task'); });
-Route::get('/schedule', function () { return view('schedule'); });
-Route::get('/user', function () { return view('users/create'); });
-Route::get('/setting', function () { return view('setting'); });
 
-
+// Users
 Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
 Route::get('/users/edit', [UserController::class, 'edit'])->name('users.edit');
 Route::get('/users/delete', [UserController::class, 'delete'])->name('users.delete');
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
 Route::post('/users', [UserController::class, 'store'])->name('users.store');
 
-
+// Schedule
 Route::get('/schedule', [ScheduleController::class, 'index'])->name('users.schedule');
