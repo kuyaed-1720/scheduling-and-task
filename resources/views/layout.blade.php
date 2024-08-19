6ff26b0b314fdaddba86bfa93fdb145f25630f7e
unknown <kuya.ed20@gmail.com>
2024-08-19 13:39:01 +0800

    Initial commit to home page

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,9 +8,19 @@
     <title>@yield('title')</title>
 </head>
 <body>
-    <header>
-        Scheduling and Task Management System
+    <header class="navbar bg-body-tertiary">
+        <h3>Scheduling and Task Management System</h3>
+        <div class="container-fluid">
+            <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
     </header>
+    <nav class="sidebar">
+        <a href="/home">Home</a>
+        <a href="/task">Task</a>
+    </nav>
     @yield('content')
 </body>
 </html>
