3dff4d1a2b1ada2d13787351dec437f7fcd0dcfe
kuyaed-1720 <kuya.ed20@gmail.com>
2024-08-21 16:45:05 +0800

    to backrooms

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -17,7 +17,7 @@
         <div class="">
             <form class="d-flex" role="search">
             <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
+            <button class="btn btn-outline-success" type="submit"><a href="https://youtu.be/dQw4w9WgXcQ">Search</a></button>
             </form>
         </div>
     </header>
