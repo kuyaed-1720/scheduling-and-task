a0b47c1de5dfdafd5a3952afff2de4b9fbf496d4
kuyaed-1720 <kuya.ed20@gmail.com>
2024-08-21 15:13:41 +0800

    try lang

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -5,5 +5,15 @@
 @endsection
 
 @section('content')
-    
+    <h1>Task Management</h1>
+    <h2>Task List</h2>
+
+    {{-- show success notif --}}
+    @if (session('success'))
+        <div class="alert alert-success">
+            {{ sessio('success') }}
+        </div>
+    @endif
+
+    {{-- Task container --}}
 @endsection
