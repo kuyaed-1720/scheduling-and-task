341cfc2fa3ea03c95bc25931d59dc8c59e7f7588
kuyaed-1720 <kuya.ed20@gmail.com>
2024-08-21 13:37:00 +0800

    Edited tasks table migration

--- database/migrations/2024_08_21_053416_create_tasks_table.php
+++ database/migrations/2024_08_21_053416_create_tasks_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->dateTime('start');
+            $table->dateTime('end')->nullable();
             $table->timestamps();
         });
     }
