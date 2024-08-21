a4ad3e14debeba0002bf590d6223d95bac6c8128
kuyaed-1720 <kuya.ed20@gmail.com>
2024-08-21 13:45:19 +0800

    Modified changes

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Task;
 
 class TaskController extends Controller
 {
-    //
+    public function index()
+    {
+        $tasks = Task::where('completed', false)->orderBy('priority', 'desc')->orderBy('end')->get();
+    }
 }
--- database/migrations/2024_08_21_053416_create_tasks_table.php
+++ database/migrations/2024_08_21_053416_create_tasks_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('title');
             $table->text('description');
             $table->dateTime('start');
-            $table->dateTime('end')->nullable();
+            $table->dateTime('end');
             $table->timestamps();
         });
     }
