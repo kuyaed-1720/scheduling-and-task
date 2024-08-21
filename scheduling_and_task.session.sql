d02f0fee00a8397c55bdcf0b6c44470102b8c394
kuyaed-1720 <kuya.ed20@gmail.com>
2024-08-21 15:01:38 +0800

    Inserted into tasks

---
+++ scheduling_and_task.session.sql
@@ -0,0 +1,50 @@
+INSERT INTO tasks (
+		title,
+		description,
+		priority,
+		due,
+		completed,
+		completed_at,
+		created_at,
+		updated_at
+	)
+values (
+		'Task 132',
+		'Descejdogierhgoi',
+		'high',
+		'2024-08-20',
+		0,
+		null,
+		now(),
+		now()
+	),
+	(
+		'Task 493',
+		'jffghh jghgrfru',
+		'high',
+		'2024-07-20',
+		1,
+		now(),
+		now(),
+		now()
+	),
+	(
+		'Task 345',
+		'fefg',
+		'low',
+		'2024-08-24',
+		0,
+		null,
+		now(),
+		now()
+	),
+	(
+		'Task 835',
+		'rgh  rgrgr',
+		'medium',
+		'2024-06-22',
+		1,
+		'2024-06-23',
+		now(),
+		now()
+	)
