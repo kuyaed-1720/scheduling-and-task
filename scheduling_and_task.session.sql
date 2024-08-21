4d561591214728f8e258bbc2a4d004c16bafafb7
kuyaed-1720 <kuya.ed20@gmail.com>
2024-08-21 16:36:28 +0800

    Try auto database

--- scheduling_and_task.session.sql
+++ scheduling_and_task.session.sql
@@ -9,37 +9,7 @@ INSERT INTO tasks (
 		updated_at
 	)
 values (
-		'Task 132',
-		'Descejdogierhgoi',
-		'high',
-		'2024-08-20',
-		0,
-		null,
-		now(),
-		now()
-	),
-	(
-		'Task 493',
-		'jffghh jghgrfru',
-		'high',
-		'2024-07-20',
-		1,
-		now(),
-		now(),
-		now()
-	),
-	(
-		'Task 345',
-		'fefg',
-		'low',
-		'2024-08-24',
-		0,
-		null,
-		now(),
-		now()
-	),
-	(
-		'Task 835',
+		'Task Ceasear',
 		'rgh  rgrgr',
 		'medium',
 		'2024-06-22',
