f92c8c0567a4ce39c89a8ecdaf0cad4b6827717e
kuyaed-1720 <kuya.ed20@gmail.com>
2024-08-22 10:16:36 +0800

    Added destroy task function

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -59,4 +59,11 @@ class TaskController extends Controller
 
         return redirect()->route('tasks.index')->with('success', 'Task updated successfully!');
     }
+
+    public function destroy(Task $task)
+    {
+        $task->delete();
+        
+        return redirect()->route('tasks.index')->with('success', 'Task deleted successfully! Mwehehhe');
+    }
 }
