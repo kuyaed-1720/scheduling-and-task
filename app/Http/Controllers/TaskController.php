c9e243060622249a52c1b50f3981e1367832c8d2
kuyaed-1720 <kuya.ed20@gmail.com>
2024-08-22 09:00:12 +0800

    Modified TaskController and Task Model + routes

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -13,4 +13,27 @@ class TaskController extends Controller
 
         return view('tasks.index', compact('tasks'));
     }
+
+    public function create()
+    {
+        return view('tasks.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|max:255',
+            'description' => 'nullable',
+            'priority' => 'required|max:255',
+            'due' => 'nullable|max:255',
+        ]);
+        Task::create([
+            'title' => $request->input('title'),
+            'description' => $request->input('description'),
+            'priority' => $request->input('priority'),
+            'due' => $request->input('due'),
+        ]);
+
+        return redirect()->route('tasks.index')->with('success', 'Task created successfully!');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,5 @@ Route::get('/events', [EventController::class, 'index'])->name('events.index');
 
 // Tasks
 Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
+Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
+Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
