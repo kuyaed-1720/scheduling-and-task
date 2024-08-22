a149c59d79cfe04c9dcb5b30a541f340a7bf522d
Ceasar082201 <refermosoc@gmail.com>
2024-08-22 08:49:43 +0800

    main texts and samples exchange

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,4 +6,14 @@
 
 @section('content')
     <h1> Welcome to Scheduling and Task Management System </h1>
+    <div class="main">
+        <div class="wrapper">
+            <div class="block-1">
+            <img src="https://image3.slideserve.com/5443844/legislative-schedule-20031-l.jpg" alt="2003 HPA Bill">
+            </div>
+            <div class="block-2">
+                <img src="https://cdn.videos.rollcall.com/author/2016/11/30174358/2017CQRC-CongressionalCalendar.jpg?utm_content=bufferd18cc&utm_medium=social&utm_source=twitter.com&utm_campaign=buffer" alt="Calendars">
+            </div>
+        </div>
+    </div>
 @endsection
