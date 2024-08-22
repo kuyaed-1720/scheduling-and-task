171fe1441804ddffb50b6b051d44b7e3d4e54a2a
kuyaed-1720 <kuya.ed20@gmail.com>
2024-08-22 09:09:06 +0800

    Added create task page

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,3 +1,35 @@
-<div>
-    <!-- If you do not have a consistent goal in life, you can not live it in a consistent way. - Marcus Aurelius -->
-</div>
+@extends('layout')
+
+@section('title')
+    Create Task
+@endsection
+
+@section('content')
+    <div class="container">
+        <h1>Create Task</h1>
+        <form action="{{ route('tasks.store') }}" method="POST">
+            @csrf
+            <div class="form-group">
+                <label for="title">Title</label>
+                <input type="text" name="title" id="title" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label for="description">Description</label>
+                <textarea name="description" id="description" cols="30" rows="10" class="form-control"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="priority">Priority</label>
+                <select name="priority" id="priority" class="form-control" required>
+                    <option value="low">Low</option>
+                    <option value="medium">Medium</option>
+                    <option value="high">High</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="due">Due</label>
+                <input type="date" class="form-control" id="due" name="due">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+@endsection
