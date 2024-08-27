41bcec6e817ceaceb9b14cd133d1d2d8c7dfaf9f
kuyaed-1720 <kuya.ed20@gmail.com>
2024-08-28 06:33:28 +0800

    Updated app-layout content

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -1,3 +1,16 @@
-<div>
-    <!-- Knowing is not enough; we must apply. Being willing is not enough; we must do. - Leonardo da Vinci -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- @include('global-plugins') --}}
+    @yield('meta')
+    <title>@yield('title')</title>
+</head>
+<body>
+    {{-- @include('header') --}}
+    {{-- @include('body') --}}
+    {{-- @include('footer') --}}
+</body>
+</html>
