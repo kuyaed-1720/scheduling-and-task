2c6727322403d3bbfc442bbc983f6b56313caa5e
kuyaed-1720 <kuya.ed20@gmail.com>
2024-08-29 11:33:16 +0800

    Added more links and fixed styles

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -1,3 +1,5 @@
 <a class="btn btn-success" href="#" role="button">Home</a>
 <a class="btn btn-success" href="#" role="button">Tasks</a>
 <a class="btn btn-success" href="#" role="button">Calendar</a>
+<a class="btn btn-success" href="#" role="button">Users</a>
+<a class="btn btn-success" href="#" role="button">Settings</a>
