161747b6535c4518651a5b18b4e5e1a30c5072e3
kuyaed-1720 <kuya.ed20@gmail.com>
2024-08-29 12:57:16 +0800

    Updated route names

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 // sidebar
 Route::get('/', function () { return view('welcome'); })->name('welcome');
 Route::get('/home', function () { return view('welcome'); })->name('welcome');
-Route::get('/tasks', function () { return view('pages.tasks.index'); })->name('pages.tasks.index');
-Route::get('/calendar', function () { return view('pages.calendar.index'); })->name('pages.calendar.index');
-Route::get('/users', function () { return view('pages.users.index'); })->name('pages.users.index');
-Route::get('/settings', function () { return view('pages.settings.index'); })->name('pages.settings.index');
+Route::get('/tasks', function () { return view('pages.tasks.index'); })->name('tasks');
+Route::get('/calendar', function () { return view('pages.calendar.index'); })->name('calendar');
+Route::get('/users', function () { return view('pages.users.index'); })->name('users');
+Route::get('/settings', function () { return view('pages.settings.index'); })->name('settings');
