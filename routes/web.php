8034892b104d99daa8c03f4b567a4af196d4d593
kuyaed-1720 <kuya.ed20@gmail.com>
2024-08-29 12:34:34 +0800

    Added main routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () { return view('welcome'); })->name('welcome');
+Route::get('/home', function () { return view('welcome'); })->name('welcome');
+Route::get('/tasks', function () { return view('pages.tasks.index'); })->name('pages.tasks.index');
+Route::get('/calendar', function () { return view('pages.calendar.index'); })->name('pages.calendar.index');
+Route::get('/users', function () { return view('pages.users.index'); })->name('pages.users.index');
+Route::get('/settings', function () { return view('pages.settings.index'); })->name('pages.settings.index');
