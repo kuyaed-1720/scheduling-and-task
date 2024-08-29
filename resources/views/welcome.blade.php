11fcd93a1e89db44ca17ab98cf20a39cd89c83d0
kuyaed-1720 <kuya.ed20@gmail.com>
2024-08-29 08:48:08 +0800

    applied layout to welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,7 @@
-<h1> Welcome to Scheduling and Task Management System </h1>
+@extends('layouts.default')
+@section('title')
+    Welcome Page
+@endsection
+@section('content')
+    <h1> Welcome to Scheduling and Task Management System </h1>
+@endsection
