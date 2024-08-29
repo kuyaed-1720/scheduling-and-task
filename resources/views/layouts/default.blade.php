d901b63279dcfa7009cb22ddd3df7544b3abc901
kuyaed-1720 <kuya.ed20@gmail.com>
2024-08-29 08:39:00 +0800

    Created default layout

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,3 +1,18 @@
-<div>
-    <!-- An unexamined life is not worth living. - Socrates -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    @include('includes.plugins')
+</head>
+<body>
+    <header>@include('includes.header')</header>
+    <div>
+        @include('includes.sidebar')
+        <main>@yield('content')</main>
+    </div>
+    <footer>@include('includes.footer')</footer>
+</body>
+</html>
