bc49f1143d2fdac6648d29cdfcb1b7e2212b4672
kuyaed-1720 <kuya.ed20@gmail.com>
2024-08-29 11:30:13 +0800

    Fixed header style, added sidebar links and styling

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,5 +1,5 @@
 <h2>Scheduling and Task Management System</h2>
 <form class="d-flex" role="search" method="GET">
     <input class="form-control me-2" name="search" id="search" type="search" placeholder="Search" aria-label="Search">
-    <button class="btn btn-primary" type="submit"><a class="search-btn">Search</a></button>
+    <button class="btn btn-outline-success" type="submit"><a class="search-btn">Search</a></button>
 </form>
---
+++ resources/views/includes/sidebar.blade.php
@@ -0,0 +1,3 @@
+<a class="btn btn-success" href="#" role="button">Home</a>
+<a class="btn btn-success" href="#" role="button">Tasks</a>
+<a class="btn btn-success" href="#" role="button">Calendar</a>
