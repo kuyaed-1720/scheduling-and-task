f258fe8042f6f06ad763c0c578f3a46c65e3b58c
kuyaed-1720 <kuya.ed20@gmail.com>
2024-08-29 09:56:32 +0800

    Fixed plugin of css

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
     @include('includes.plugins')
-    @vite('resources/css.app.css', 'resources/js/app.js')
+    @vite('resources/css/app.css', 'resources/js/app.js')
 </head>
 <body class="text-center">
     <header class="row">
