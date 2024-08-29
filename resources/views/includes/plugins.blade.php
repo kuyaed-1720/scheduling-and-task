3e8dcf0b335dc3a85070c91ab34aefc167012601
kuyaed-1720 <kuya.ed20@gmail.com>
2024-08-29 09:20:07 +0800

    Added bootstrap bundle and icons, fontawesome, and jquery cdn links

--- resources/views/includes/plugins.blade.php
+++ resources/views/includes/plugins.blade.php
@@ -1,3 +1,11 @@
-<div>
-    <!-- Let all your things have their places; let each part of your business have its time. - Benjamin Franklin -->
-</div>
+{{-- bootstrap@5.3.2 --}}
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+{{-- bootstrap-icons@1.11.1 --}}
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" integrity="sha384-4LISF5TTJX/fLmGSxO53rV4miRxdg84mZsxmO8Rx5jGtp/LbrixFETvWa5a6sESd" crossorigin="anonymous">
+
+{{-- fontawesome-free@6.4.2 --}}
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.4.2/css/fontawesome.min.css" integrity="sha384-BY+fdrpOd3gfeRvTSMT+VUZmA728cfF9Z2G42xpaRkUGu2i3DyzpTURDo5A6CaLK" crossorigin="anonymous">
+
+{{-- jquery@1.11.1 --}}
+<script src="http://code.jquery.com/jquery-latest.js"></script>
