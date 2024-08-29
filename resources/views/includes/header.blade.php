a947a07ff3081d7ba58f964eac3a4029123585f3
kuyaed-1720 <kuya.ed20@gmail.com>
2024-08-29 11:09:39 +0800

    Added header content and styling

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,5 @@
+<h2>Scheduling and Task Management System</h2>
+<form class="d-flex" role="search" method="GET">
+    <input class="form-control me-2" name="search" id="search" type="search" placeholder="Search" aria-label="Search">
+    <button class="btn btn-primary" type="submit"><a class="search-btn">Search</a></button>
+</form>
