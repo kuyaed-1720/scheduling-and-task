f7ae17c7e5d3fab3f51f2b16f16de1063e75282e
Ceasar082201 <refermosoc@gmail.com>
2024-09-10 15:45:32 +0800

    approve the adjustments

--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -3,12 +3,9 @@
     setting
 @endsection
 @section('content')
-<div style="display:flex; flex-direction:column; text-align:center;">
-    <button><a href="/home">Home</a></button>
-    <button><a href="/task">Task</a></button>
-    <button><a href="/events">Events</a></button>
-    <button><a href="/user">User</a></button>
-    <button></button><a href="#!">Unknown_0</a>
-    <a href="#!">Unknown_1</a>
+<div class="set_0">
+    <div class="set_1">Setting</div>
+    <div class="set_2">Account</div>
+    <div class="set_3">About</div>
 </div>
 @endsection
