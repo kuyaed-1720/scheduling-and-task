0b04e42c8067cb417fde90eac274a138c811baaa
Ceasar082201 <refermosoc@gmail.com>
2024-09-12 09:50:19 +0800

    commit now or never

--- resources/js/calendar.js
+++ resources/js/calendar.js
@@ -18,13 +18,38 @@ document.addEventListener('DOMContentLoaded', function () {
 	const calendar = new Calendar(calendarEl, {
 		plugins: [dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin],
 		initialView: 'dayGridMonth',
+		headerToolbar: { 
+			left:'timeGridWeek,dayGridMonth,listWeek',
+			center: 'title'},
 		editable: true,
 		selectable: true,
 		events: '/api/events',
-		dateClick: function (info) {
-			$('#eventModal').modal('show');
-			// $('#start').val(info.dateStr);
+		timeZone: 'Manila/Philippines',
+		eventColor:'black',
+		eventDisplay:'auto',
+		stickyHeaderDates:'true',
+		handleWindowResize: true,
+		businessHours:[
+			{
+				daysOfWeek:[ 1, 2, 3, 4, 5],
+				startTime: '8:00',
+				eventDisplay: 'list-items',
+				endTime: '17:00',
+			}
+		],
+		dateClick: function(info){
+			alert('CLICKED: ' + formatIsoTimeString);
+		},
+		// eventClick: function(info){
+		// 	alert('.eventModal' + info.events)
+		// },
+		
+		dayMaxEventRows: true,
+		views:{
+			timeGrid:{
+				dayMaxEventRows: 5
+			}
 		}
-	});
+		});
 	calendar.render();
 });
--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -4,8 +4,8 @@
 @endsection
 @section('content')
 <div class="set_0">
-    <div class="set_1">Setting</div>
-    <div class="set_2">Account</div>
-    <div class="set_3">About</div>
+    <div class="set_1"><button class="set_1">Setting</button></div>
+    <div class="set_2"><button class="set_2">Account</button></div>
+    <div class="set_3"><button class="set_3">About</button></div>
 </div>
 @endsection
